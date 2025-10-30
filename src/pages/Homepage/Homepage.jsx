b1a72488a3ef0e5d7055ca04d146eb8e3f2d87ba
Malgotech <malgotechnologiespvtltd@gmail.com>
2025-10-30 13:21:35 +0530

    add

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -11,7 +11,7 @@ const Homepage = () => {
   return (
      <div className={Style.homepage}>
       <PremiumCopper/>
-      <LondonMetal/>
+      {/* <LondonMetal/> */}
       <WhatWeDo/>
       <GlobalSupply/>
       <PartnerShip/>
