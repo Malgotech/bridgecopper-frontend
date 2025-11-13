49a31fee729bad2e51e8b33bbeb1bff761ef3e8a
Malgotech <malgotechnologiespvtltd@gmail.com>
2025-11-13 16:13:20 +0530

    333333333

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -15,8 +15,8 @@ const Homepage = ({ isActive,handleContactModal,contactModal,setContactModal }) 
     <div className={Style.homepage}>
       <BannerSection handleContactModal={handleContactModal}/>
       <PremiumCopper />
-      {/* <LondonMetal />
-      <WhatWeDo isActive={isActive} />
+      <LondonMetal />
+      {/* <WhatWeDo isActive={isActive} />
 
       <div className= {Style.supply_container} id="network">
         <GlobalSupply />
